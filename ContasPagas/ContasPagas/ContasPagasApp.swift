d5d07f6784e18b5f3f0318b539784be00a8f2802
Ronaldo Gomes <gomes.ronaldo96@gmail.com>
2023-03-19 00:58:36 -0300

    Add gitignore

---
+++ ContasPagas/ContasPagas/ContasPagasApp.swift
@@ -0,0 +1,20 @@
+//
+//  ContasPagasApp.swift
+//  ContasPagas
+//
+//  Created by Ronaldo Gomes on 19/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ContasPagasApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
